5b92cc58e016ee2bcaef549f9594e1ac307224ca
iterna1 <fortun.ik@yandex.ru>
2018-12-15 11:47:32 +0500

    Создание модуля для рассчёта точек графиков

--- graph_ticks.py
+++ graph_ticks.py
@@ -1,45 +1,46 @@
-import calendar
 import datetime
 
 
-def main():
-    birth_d = list(map(int, input('Введите дату (D/Y/M): ').split('/')))
-    td = TimeDelta(birth_d)
-    t = td.delta.days
-    print(t)
+class BadDateFormat(Exception):
+    pass
 
 
 class TimeDelta:
     def __init__(self, birth_date):
-        self.birth_date = datetime.datetime(*birth_date[::-1])
-        self.current_date = datetime.datetime.now()
-        self.delta = self.current_date - self.birth_date
+        self.birth_date = self.birth_date(birth_date)
+        self.current_date = self.current_date()
+        self.delta = self.get_delta()
+
+    def birth_date(self, birth_date):
+        try:
+            if birth_date.count('/') != 2:
+                raise BadDateFormat
+
+            birth_date = birth_date.split('/')
+
+            if not ''.join(birth_date).isdigit():
+                raise BadDateFormat
+            birth_date = map(int, birth_date)
+
+        except BadDateFormat:
+            raise BadDateFormat('Введите дату в формате ДД/ММ/ГГГГ')
+        else:
+            return datetime.datetime(*list(birth_date)[::-1])
+
+    def current_date(self):
+        return datetime.datetime.now()
+
+    def get_delta(self):
+        return self.current_date - self.birth_date
 
     def seconds(self):
         return self.delta.seconds
 
-    def years(self):
-        days = 0
-        year = self.birth_date
-        dy = 365
-        count = [0, 0]
-        if calendar.isleap(year.year):
-            dy = 366
-            count[1] += 1
-        while days + dy < self.delta.days:
-            days += dy
-            bb = datetime.timedelta(days=dy)
-            year = year + bb
-            if calendar.isleap(year.year):
-                dy = 366
-                count[1] += 1
-            else:
-                dy = 365
-            count[0] += 1
-        return count
 
-    def delta_birthday_today(self):
-        return abs(self.birth_date.day - self.current_date.day)
+def main():
+    birth_d = input('Введите дату (D/Y/M): ')
+    td = TimeDelta(birth_d)
+    print(td.get_delta().days)
 
 
 if __name__ == '__main__':
