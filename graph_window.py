5817181761ff4d71fff6775ea42da83c93c5ebab
iterna1 <fortun.ik@yandex.ru>
2018-12-16 01:31:14 +0500

    Нужно доработать graph_window

--- graph_window.py
+++ graph_window.py
@@ -1,28 +1,66 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
 from style import Style
 
 
-class GraphWindow(QWidget):
+class GraphWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
         self.stl = Style(self)
+        self.initUI()
 
     def initUI(self):
+        table_widget = TableWidget(self)
+        self.setCentralWidget(table_widget)
+
+
+class TableWidget(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        stl = Style(self)
+
+        layout = QVBoxLayout(self)
+        tabs = stl.get_tab_style({'Параметры': SetupTab(), 'График': GraphTab()})
+
+        layout.addWidget(tabs)
+        self.setLayout(layout)
+
+
+class SetupTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.stl = Style(self)
+        self.initUi()
+
+    def initUi(self):
         self.stl.get_background_style()
-        check_box = [self.stl.get_checkbox_style('Физический', 8),
-                     self.stl.get_checkbox_style('Эмоциональный', 8),
-                     self.stl.get_checkbox_style('Интеллектуальный', 8)]
+        check_box = [self.stl.get_label_style('Отображать:', 16),
+                     self.stl.get_checkbox_style('Физический биоритм', 12, 'green'),
+                     self.stl.get_checkbox_style('Эмоциональный биоритм', 12, 'red'),
+                     self.stl.get_checkbox_style('Интеллектуальный биоритм', 12, 'blue'),
+                     self.stl.get_checkbox_style('Среднее значение', 12, 'black')]
+        vbox = QVBoxLayout(self)
+        [vbox.addWidget(cb) for cb in check_box]
 
 
+class GraphTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.stl = Style(self)
+        self.initUi()
+
+    def initUi(self):
+        self.stl.get_background_style()
 
 
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    gw = GraphWindow()
+    gw.show()
+    sys.exit(app.exec())
 
 if __name__ == 'main_window':
-    print(globals())
     app = QApplication(sys.argv)
     gw = GraphWindow()
     gw.show()
--- style.py
+++ style.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtWidgets import QLabel, QPushButton, QCheckBox
+from PyQt5.QtWidgets import QLabel, QPushButton, QCheckBox, QTabWidget
 
 
 class Style:
@@ -25,9 +25,17 @@ class Style:
         label.adjustSize()
         return label
 
-    def get_checkbox_style(self, text, font):
+    def get_checkbox_style(self, text, font, color):
         check_box = QCheckBox(text, self.obj)
-        check_box.setStyleSheet('QCheckBox{color: #8B0000;}')
+        check_box.setStyleSheet('QCheckBox{background-color: #F5F5DC; color: %s;}' % color)
         check_box.setFont(QFont('Consolas', font, QFont.Bold))
         check_box.adjustSize()
         return check_box
+
+    def get_tab_style(self, kwargs):
+        tabs = QTabWidget()
+        for i in kwargs:
+            tabs.addTab(kwargs[i], i)
+        tabs.adjustSize()
+        tabs.setStyleSheet('QTabWidget{background-color: #F5F5DD;}')
+        return tabs
